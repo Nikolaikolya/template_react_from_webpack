dfee305b846a54299f987754078d2ef97caaeaf0
Mikolai Fedotov <starcrafters@ya.ru>
2022-04-23 19:38:56 +0700

    Add rules load image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import * as React from 'react';
+import './style.css';
+import Image from './color.png';
+import Icon from './i.svg';
 
 export const App = () => {
-  return <h1>Hello</h1>
+  return (
+    <>
+      <h1>Hello</h1>
+      <img src={Image} alt="" />
+      <img src={Icon} alt="" />
+    </>
+  )
 }
